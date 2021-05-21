2c70899c3d79bbb6263143177289f6e324603fd8
Terry <incubus8@gmail.com>
2021-05-21 15:40:06 -0400

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
 });
+
+document.querySelector('#create-task-form').addEventListener('submit', createNewList)
+let form = document.querySelector('#create-task-form')
+
+function createNewList (e) {
+  e.preventDefault()
+  let li = document.createElement('li')
+    li.textContent = form.children[1].value
+    document.querySelector('#tasks').appendChild(li)
+    console.log(e.target['new-task-description'].value)
+}
+
