822058ed7672ae038b1dcc47dad73187b0db3cb5
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-21 16:09:59 -0400

    kenny commit

--- src/index.js
+++ src/index.js
@@ -8,8 +8,12 @@ let form = document.querySelector('#create-task-form')
 function createNewList (e) {
   e.preventDefault()
   let li = document.createElement('li')
-    li.textContent = form.children[1].value
-    document.querySelector('#tasks').appendChild(li)
-    console.log(e.target['new-task-description'].value)
+  li.textContent = form.children[1].value
+  document.querySelector('#tasks').appendChild(li)
+  // console.log(e.target['new-task-description'].value)
+  let delBtn = document.createElement('button')
+  li.appendChild(delBtn)
+  delBtn.textContent = "X"
+  delBtn.addEventListener('click', ()=>(li.remove()))
+  document.getElementById("create-task-form").reset()
 }
-
